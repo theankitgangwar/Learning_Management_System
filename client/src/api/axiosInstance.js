60f1c7a6a58e0128a7b6bfc73e67083fc49f1ea9
theankitgangwar <theankitgangwar@gmail.com>
2025-08-23 15:39:33 +0530

    updated frontend url

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://learnx-backend-5nxy.onrender.com/",
 });
 
 axiosInstance.interceptors.request.use(
